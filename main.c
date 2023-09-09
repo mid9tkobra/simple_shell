60677631f6ca419d02b3e121a6c851e5db7ac9b2
bakka22 <135711566+bakka22@users.noreply.github.com>
2023-09-09 18:26:42 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,72 +8,65 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <stddef.h>
-void interactive(char *buf, char *buf2, char **args)
+void interactive(char *buf, char *buf2, char **args, char **env)
 {
 	int status, j, argcou, x;
 	size_t i;
 	pid_t child;
-	
-	while (1)
-		{
-			write(STDOUT_FILENO, buf2, 2);
-			x = read(STDIN_FILENO, buf, 1024);
-			if (x == 0)
-				return (0);
-			if (buf[(x - 1)] == '\n')
-				buf[(x - 1)] = '\0';
-			argcou = argcount(buf);
-			args = malloc(argcou * sizeof(char *));
-			buf = strtok(buf, " ");
-			args[0] = buf;
-			for (j = 1; j < argcou; j++)
-			{
-				args[j] = strtok(NULL, " ");
-			}
-			(void)args;
-			child = fork();
-			if (child == -1)
-			{
-				perror("Error:\n");
-				exit(1);
-			}
 
-			if (child == 0)
-			{
-				if (strcmp(buf, "cd") == 0)
-					chdir(args[1]);
-				else
-					execve(buf, args, environ);
-			}
-			else
-			{
-				wait(&status);
-			}
+	while (1)
+	{
+		write(STDOUT_FILENO, buf2, 2);
+		x = read(STDIN_FILENO, buf, 1024);
+		if (x == 0)
+			return (0);
+		if (buf[(x - 1)] == '\n')
+			buf[(x - 1)] = '\0';
+		argcou = argcount(buf);
+		args = malloc(argcou * sizeof(char *));
+		buf = strtok(buf, " ");
+		args[0] = buf;
+		for (j = 1; j < argcou; j++)
+		{
+			args[j] = strtok(NULL, " ");
 		}
+		child = fork();
+		if (child == 0)
+		{
+			if (strcmp(buf, "cd") == 0)
+				chdir(args[1]);
+			else
+				execve(buf, args, env);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
 }
 int main (int ac, char **av, char **env)
 {
 	pid_t parent;
 	int i = 0, j = 1;
 	char *buf, **args, *buf2 = "$ ";
-	extern char **environ;
 	parent = getpid();
+
 	buf = malloc(sizeof(char) * 1024);
 	if (ac == 1)
 	{
-		interactive(buf, buf2, args);
+		interactive(buf, buf2, args, env);
 	}
 	else
 	{
 		args = malloc(sizeof(char *) * (ac - 1);
-		while(av[j])
+		while (av[j])
 		{
 			args[i] = av[j];
 			i++;
 			j++;
 		}
 		buf = args[0];
-		execve(buf, args, environ);
+		execve(buf, args, env);
 	}
 	return (0);
 }
