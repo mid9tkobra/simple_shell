9c770198e05d1d3bc7309b8c0547813379940f54
bakka22 <abobakereltom@gmail.com>
2023-09-09 16:24:26 +0000

    s

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ void interactive(char *buf, char *buf2, char **args, char **env)
 		write(STDOUT_FILENO, buf2, 2);
 		x = read(STDIN_FILENO, buf, 1024);
 		if (x == 0)
-			return (0);
+			return;
 		if (buf[(x - 1)] == '\n')
 			buf[(x - 1)] = '\0';
 		argcou = argcount(buf);
@@ -28,35 +28,6 @@ void interactive(char *buf, char *buf2, char **args, char **env)
 		args[0] = buf;
 		for (j = 1; j < argcou; j++)
 		{
-			write(STDOUT_FILENO, buf2, 2);
-			x = read(STDIN_FILENO, buf, 1024);
-			if (x == 0)
-				return;
-			if (buf[(x - 1)] == '\n')
-				buf[(x - 1)] = '\0';
-			argcou = argcount(buf);
-			args = malloc(argcou * sizeof(char *));
-			buf = strtok(buf, " ");
-			args[0] = buf;
-			for (j = 1; j < argcou; j++)
-			{
-				args[j] = strtok(NULL, " ");
-			}
-			(void)args;
-			child = fork();
-			if (child == -1)
-			{
-				perror("Error:\n");
-				exit(1);
-			}
-
-			if (child == 0)
-			{
-				if (strcmp(buf, "cd") == 0)
-					chdir(args[1]);
-				else
-					execve(buf, args, environ);
-			}
 			args[j] = strtok(NULL, " ");
 		}
 		child = fork();
@@ -73,13 +44,13 @@ void interactive(char *buf, char *buf2, char **args, char **env)
 		}
 	}
 }
-int main (int ac, char **av, char **env)
+int main(int ac, char **av, char **env)
 {
 	pid_t parent;
 	int i = 0, j = 1;
 	char *buf, **args, *buf2 = "$ ";
-	parent = getpid();
 
+	parent = getpid();
 	buf = malloc(sizeof(char) * 1024);
 	if (ac == 1)
 	{
@@ -89,9 +60,6 @@ int main (int ac, char **av, char **env)
 	{
 
 		args = malloc(sizeof(char *) * (ac - 1));
-		while(av[j])
-
-		args = malloc(sizeof(char *) * (ac - 1);
 		while (av[j])
 		{
 			args[i] = av[j];
